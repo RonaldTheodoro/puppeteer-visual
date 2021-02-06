62b6487e381a70448d36088135a4f139ddc3f773
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-06 17:08:46 -0300

    40. Responsive Visual Tests

--- tests-snapshots/__tests__/visual.test.js
+++ tests-snapshots/__tests__/visual.test.js
@@ -35,4 +35,26 @@ describe('Visual regression testing', () => {
       failureThreshold: 0.01,
     })
   })
+
+  test('Mobile snapshot', async () => {
+    await page.goto('http://www.example.com')
+    await page.waitForSelector('h1')
+    await page.emulate(puppeteer.devices['iPhone X'])
+    const image = await page.screenshot()
+    expect(image).toMatchImageSnapshot({
+      failureThresholdType: 'percent',
+      failureThreshold: 0.01,
+    })
+  })
+
+  test('Tablet snapshot', async () => {
+    await page.goto('http://www.example.com')
+    await page.waitForSelector('h1')
+    await page.emulate(puppeteer.devices['iPad landscape'])
+    const image = await page.screenshot()
+    expect(image).toMatchImageSnapshot({
+      failureThresholdType: 'percent',
+      failureThreshold: 0.01,
+    })
+  })
 })
